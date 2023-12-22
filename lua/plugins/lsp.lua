fcfc10ff527c83b9d6411f34ceed7427d66937da
Peter Benjamin <petermbenjamin@gmail.com>
2023-12-21 16:47:28 -0800

    feat(lsp): add snyk-ls configuration

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,7 +2,16 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
-      snyk_ls = {},
+      snyk_ls = {
+        filetypes = { "javascript", "json", "yaml", "go" },
+        init_options = {
+          activateSnykCode = "true",
+          activateSnykIac = "true",
+          activateSnykOpenSource = "true",
+          enableTrustedFoldersFeature = "false",
+          organization = "komodohealth",
+        },
+      },
     },
   },
 }
