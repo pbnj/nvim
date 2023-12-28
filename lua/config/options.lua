701ce0f896bed9f4a9621a87bb7f975a845e64e9
Peter Benjamin <petermbenjamin@gmail.com>
2023-12-27 20:15:19 -0800

    fix(plugins): use netrw instead of neo-tree

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,9 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-local opt = vim.opt
 
-opt.relativenumber = false
-opt.splitbelow = false
-opt.splitright = false
-opt.wildmode = { "full" }
+vim.g.netrw_keepdir = false
+vim.opt.relativenumber = false
+vim.opt.splitbelow = false
+vim.opt.splitright = false
+vim.opt.wildmode = { "full" }
