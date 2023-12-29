3ff52b37474c30c4d4c9ca30a3ed99bf487bd836
Peter Benjamin <petermbenjamin@gmail.com>
2023-12-29 12:42:21 -0800

    feat(ddgr): support command args

--- plugin/ddgr.lua
+++ plugin/ddgr.lua
@@ -3,8 +3,8 @@ if vim.g.loaded_ddgr == 1 then
 end
 vim.g.loaded_ddgr = 1
 
-local LazyUtil = require("lazyvim.util")
+local LazyTerm = require("lazyvim.util.terminal")
 
-vim.api.nvim_create_user_command("DDGR", function()
-  LazyUtil.terminal.open("ddgr")
+vim.api.nvim_create_user_command("DDGR", function(opts)
+  LazyTerm.open(vim.tbl_flatten({ "ddgr", opts.fargs }))
 end, { nargs = "*" })
