9e674bde6c3e1537608fad164599b0a1bf73b2bb
Peter Benjamin <petermbenjamin@gmail.com>
2023-12-31 15:56:08 -0800

    chore: remove vim-helm

--- lua/plugins/helm.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "towolf/vim-helm",
-  -- ft = { "yaml" },
-  event = {
-    "BufRead */templates/*.yaml,*/templates/*.tpl,*.gotmpl,helmfile*.yaml",
-    "BufNewFile */templates/*.yaml,*/templates/*.tpl,*.gotmpl,helmfile*.yaml",
-  },
-}
