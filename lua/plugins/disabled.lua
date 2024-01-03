d55217e5e824868b10d44b550f5eda96173ea0aa
Peter Benjamin <petermbenjamin@gmail.com>
2024-01-03 08:30:26 -0800

    chore: disable plugins

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,4 +3,6 @@ return {
   { "catppuccin/nvim", name = "catppuccin", enabled = false },
   { "folke/flash.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "echasnovski/mini.pairs", enabled = false },
+  { "echasnovski/mini.surround", enabled = false },
 }
