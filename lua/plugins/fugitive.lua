ce494cfc2a5a4fbb9ea9e4299da1de3e50a9354c
Peter Benjamin <petermbenjamin@gmail.com>
2024-01-05 14:44:34 -0800

    feat(fugitive): add git push keymaps

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,8 +2,27 @@ return {
   "tpope/vim-fugitive",
   dependencies = { "tpope/vim-rhubarb" },
   keys = {
-    { "<leader>gg", "<cmd>Git<cr>", desc = "Open Fugitive Git" },
-    { "<leader>gw", "<cmd>Gwrite<cr>", desc = "Git Add Current Buffer" },
+    {
+      "<leader>gg",
+      function()
+        vim.cmd("Git")
+      end,
+      desc = "Fugitive",
+    },
+    {
+      "<leader>gw",
+      function()
+        vim.cmd("Gwrite")
+      end,
+      desc = "Git Add Current Buffer",
+    },
+    {
+      "<leader>gp",
+      function()
+        vim.cmd("Git push --set-upstream origin")
+      end,
+      desc = "git push --set-upstream origin",
+    },
   },
   cmd = { "G", "Git", "Gwrite", "Gw", "GBrowse" },
 }
