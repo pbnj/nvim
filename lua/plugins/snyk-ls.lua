164adc7a17f1506ef2286f036b861eed40cc0aea
Peter Benjamin <petermbenjamin@gmail.com>
2024-01-12 14:39:23 -0800

    fix(snyk-ls): enable on terraform files

--- lua/plugins/snyk-ls.lua
+++ lua/plugins/snyk-ls.lua
@@ -3,13 +3,15 @@ return {
   opts = {
     servers = {
       snyk_ls = {
-        filetypes = { "javascript", "typescript", "go", "python" },
+        filetypes = { "javascript", "typescript", "go", "python", "terraform" },
         init_options = {
           activateSnykCode = "true",
           activateSnykIac = "true",
           activateSnykOpenSource = "true",
           additionalParams = "--all-projects",
           enableTrustedFoldersFeature = "false",
+          integrationName = "neovim",
+          filterSeverity = { critical = true, high = true, medium = false, low = false },
           organization = vim.env.SNYK_ORG,
           token = vim.env.SNYK_TOKEN,
         },
