4a7dc44fd6df4789a2ecb3db3c0533d1248596e2
Peter Benjamin <petermbenjamin@gmail.com>
2024-01-15 19:59:23 -0800

    feat(cmp): add tmux and git completion sources

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,8 +1,18 @@
 return {
-  "hrsh7th/nvim-cmp",
-  dependencies = { "andersevenrud/cmp-tmux" },
-  ---@param opts cmp.ConfigSchema
-  opts = function(_, opts)
-    table.insert(opts.sources, { name = "tmux" })
-  end,
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "andersevenrud/cmp-tmux" },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "tmux" })
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { { "petertriho/cmp-git", dependencies = "nvim-lua/plenary.nvim", opts = true } },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "git" })
+    end,
+  },
 }
