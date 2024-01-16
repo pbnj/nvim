a343cb50762e99fe8f91f56d2e42b115c907e6bd
Peter Benjamin <petermbenjamin@gmail.com>
2024-01-15 20:14:47 -0800

    revert(pairs): re-enable mini.pairs

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,5 +3,4 @@ return {
   { "catppuccin/nvim", name = "catppuccin", enabled = false },
   { "folke/flash.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-  { "echasnovski/mini.pairs", enabled = false },
 }
