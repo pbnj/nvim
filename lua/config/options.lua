7f5f119e12d6a95639a26f917124713a0fef8117
Peter Benjamin <petermbenjamin@gmail.com>
2024-02-01 15:38:39 -0800

    fix(options): set working directory to netrw directory

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- vim.g.netrw_keepdir = false
+vim.g.netrw_keepdir = false -- for telescope to work with `vim /path/to/project/dir/`
 vim.opt.conceallevel = 0
 vim.opt.relativenumber = false
 vim.opt.splitbelow = false
