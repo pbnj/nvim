755fc6067361b0dcf28f5dabbeece199d1f92f2f
Peter Benjamin <petermbenjamin@gmail.com>
2024-02-08 10:08:19 -0800

    chore: disable plugins causing performance issues

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -4,4 +4,7 @@ return {
   { "folke/flash.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "nvimdev/dashboard-nvim", enabled = false },
+  { "echasnovski/mini.indentscope", enabled = false },
+  { "nvim-treesitter/nvim-treesitter-context", enabled = false },
+  { "RRethy/vim-illuminate", enabled = false },
 }
