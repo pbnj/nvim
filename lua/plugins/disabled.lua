a330e1a2cf7638668a7fe46ee5a101ab653506c8
Peter Benjamin <petermbenjamin@gmail.com>
2024-02-08 11:58:35 -0800

    chore: re-enable dashboard & disable headlines

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,8 +3,8 @@ return {
   { "catppuccin/nvim", name = "catppuccin", enabled = false },
   { "folke/flash.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-  { "nvimdev/dashboard-nvim", enabled = false },
   { "echasnovski/mini.indentscope", enabled = false },
   { "nvim-treesitter/nvim-treesitter-context", enabled = false },
   { "RRethy/vim-illuminate", enabled = false },
+  { "lukas-reineke/headlines.nvim", enabled = false },
 }
